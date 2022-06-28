ef87ed66535d087b7cb231be3926391cd976d4fd
richnei <rich.nei@hotmail.com>
2022-06-28 10:50:46 -0300

    backend

---
+++ src/controllers/ContentController.js
@@ -0,0 +1,20 @@
+const Annotations = require('../models/AnnotationsData');
+
+module.exports = {
+
+    async update(request, response) {
+        const { id } = request.params;
+        const { notes } = request.body;
+
+        const annotation = await Annotations.findOne({ _id : id });
+
+        if (notes) {
+            annotation.notes = notes;
+
+            await annotation.save();
+        }
+
+        return response.json(annotation);
+    }
+
+}
--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ const routes = express.Router();
 
 const AnnotationController = require('./controllers/AnnotationController');
 const ImportantController = require('./controllers/ImportantController');
+const ContentController = require('./controllers/ContentController');
 
 // Rota Annotations
 routes.post('/annotations', AnnotationController.create);
@@ -11,7 +12,9 @@ routes.delete('/annotations/:id', AnnotationController.delete);
 
 // Rota Important
 routes.get('/importants', ImportantController.read);
-routes.post('/importants/:id', ImportantController.update)
+routes.post('/importants/:id', ImportantController.update);
 
+// Rota Content
+routes.post('/contents/:id', ContentController.update);
 
 module.exports = routes;
